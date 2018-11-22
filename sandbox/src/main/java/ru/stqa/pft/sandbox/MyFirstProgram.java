248ee42b55d05694a4809b297e89239f109975e6
nlkazakova <nlkazakova@users.noreply.github.com>
2018-11-22 12:51:41 +0300

    Добавлено вычисление расстояние с помощью метода класса

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,7 +8,8 @@ public class MyFirstProgram {
 		Point p1 = new Point(100, 100);
 		Point p2 = new Point(200, 100);
 
-		System.out.println("Расстояние между точками P1 (" + p1.x + "," + p1.y + ") и P2 (" + p2.x + "," + p2.y + ") равно: " + distance(p1, p2));
+		System.out.println("Расстояние между точками P1 (" + p1.x + "," + p1.y + ") и P2 (" + p2.x + "," + p2.y + ") равно: " + distance(p1, p2) + " (вычисление с помощью функции)");
+		System.out.println("Расстояние между точками P1 (" + p1.x + "," + p1.y + ") и P2 (" + p2.x + "," + p2.y + ") равно: " + p1.distance(p2) + " (вычисление с помощью метода)");
 	}
 
 	public static double distance(Point p1, Point p2) {
