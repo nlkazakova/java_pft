d38e08879af957d46bb107c1281e05b973dffe51
nlkazakova <nlkazakova@users.noreply.github.com>
2018-11-22 08:36:51 +0300

    Возведение в степень выполнено с помощью функции Math.pow

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,14 +6,14 @@ public class MyFirstProgram {
 		System.out.println("Hello, world!");
 
 		Point p1 = new Point(100, 100);
-		Point p2 = new Point(200, 200);
+		Point p2 = new Point(200, 100);
 
 		System.out.println("Расстояние между точками P1 (" + p1.x + "," + p1.y + ") и P2 (" + p2.x + "," + p2.y + ") равно: " + distance(p1, p2));
 	}
 
 	public static double distance(Point p1, Point p2) {
 
-		return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+		return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y),2));
 	}
 
 }
