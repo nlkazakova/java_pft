cbe8895de06dde72bf18a307ecbb76c80bad5c4d
nlkazakova <nlkazakova@users.noreply.github.com>
2018-11-25 23:34:23 +0300

    Задание №3. Тесты для метода distance класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -7,32 +7,32 @@ import org.testng.annotations.Test;
 public class PointTests {
 
     @Test
-    //Проверка расстояния по прямой
-    public void testDistanceLine(){
-        Point p1 = new Point(100,100);
-        Point p2 = new Point(200,100);
+    //Расстояние - целое число, точка на одной прямой справа
+    public void testDistanceIntRight(){
+        Point p1 = new Point(1000,100);
+        Point p2 = new Point(2000,100);
         Assert.assertEquals(p1.distance(p2), 100.0);
     }
 
     @Test
-    //Проверка нулевого расстояния
+    //Нулевое расстояние
     public void testDistanceZero(){
-        Point p1 = new Point(100,100);
-        Point p2 = new Point(100,100);
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,0);
         Assert.assertEquals(p1.distance(p2), 0.0);
     }
 
     @Test
-    //Проверка расстояния точки в другую сторону
-    public void testDistanceNegative(){
+    //Расстояние - целое число, точка на одной прямой слева
+    public void testDistanceIntLeft(){
         Point p1 = new Point(200,100);
         Point p2 = new Point(100,100);
         Assert.assertEquals(p1.distance(p2), 100.0);
     }
 
     @Test
-    //Проверка расстояния по диагонали
-    public void testDistanceDiagonal(){
+    //Расстояние - дробное число
+    public void testDistanceDouble(){
         Point p1 = new Point(300,0);
         Point p2 = new Point(200,100);
         Assert.assertEquals((double)Math.round(p1.distance(p2) * 100d) / 100d, 141.42);
